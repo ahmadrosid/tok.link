806637f0c5b7099387d9ee1b8f95da21777b6185
ahmadrosid <alahmadrosid@gmail.com>
2023-05-02 01:31:33 +0700

    fix: remove debug code

--- app/routes/path.ts
+++ app/routes/path.ts
@@ -43,8 +43,6 @@ export async function action({ request, context: { auth } }: ActionArgs) {
     route = nanoid(6);
   }
 
-  await new Promise((resolve, _) => setTimeout(() => resolve(true), 2000));
-
   await Path.create({
     user_id: user.id,
     title,
